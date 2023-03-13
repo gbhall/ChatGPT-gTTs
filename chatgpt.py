af8b5035bf078d55bff73c7e99561358a0003c8e
Gareth Blake Hall <gareth@gbhapps.com.au>
2023-03-14 00:07:35 +0800

    Added chatgpt.py

---
+++ chatgpt.py
@@ -0,0 +1,33 @@
+import sys
+import openai
+from cute_voice import create_cute_voice
+
+# Function that generates a ChatGPT response for a given prompt
+def generate_chatgpt_response(prompt):
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion
+
+# Function that gets the user input
+def get_user_input():
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        return input("Enter the text: ")
+
+# Driver code
+if __name__ == '__main__':
+    print(sys.argv)
+    prompt = get_user_input()
+    response = generate_chatgpt_response(prompt)
+    if response['choices'] and response['choices'][0]['message']:
+        message = response['choices'][0]['message']['content']
+        print(message)
+        create_cute_voice(message)
+    else:
+        print('Failed to generate a response from ChatGPT API.')
+        create_cute_voice("I'm having trouble connecting to the ChatGPT API.")
